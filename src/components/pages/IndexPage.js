61ec476f9f1c20640edb3784403642c7aca5477a
Mobinzaid12 <mobinzaidi2015@gmail.com>
2023-06-14 16:44:11 -0700

    14/6/2023

--- src/components/pages/IndexPage.js
+++ src/components/pages/IndexPage.js
@@ -2,36 +2,34 @@ import { useState, useEffect } from "react";
 import Post from "../Post";
 
 export default function IndexPage() {
-
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        getPost();
-    }, [])
+        fetchPosts();
+    }, []);
 
-    const getPost = async () => {
-
-        const response = await fetch("http://localhost:4500/api/post/getPost", {
-            method: "GET",
-            headers: {
-                authorization: `bearer ${JSON.parse(localStorage.getItem('token'))}`
+    const fetchPosts = async () => {
+        try {
+            const response = await fetch("http://localhost:4500/api/post/posts", {
+                headers: {
+                    authorization: `bearer ${JSON.parse(localStorage.getItem('token'))}`
+                }
+            });
+            if (!response.ok) {
+                throw new Error("Failed to fetch posts");
             }
-        });
-        if (!response.ok) {
-            throw new Error("Failed to fetch posts");
+            const data = await response.json();
+            setPosts(data);
+        } catch (error) {
+            console.error(error);
         }
-        const data = await response.json();
-        setPosts(data);
-
     };
-
 
     return (
         <>
             {posts.length > 0 && posts.map(post => (
-                <Post {...post} />
+                <Post key={post.id} {...post} />
             ))}
-
         </>
     );
 }
